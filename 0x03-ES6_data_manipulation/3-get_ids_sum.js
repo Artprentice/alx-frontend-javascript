228277671ff7107fad6902cd7108fd1f31167b15
abdullah <abdallahsuleiman56@gmail.com>
2023-07-03 21:58:33 +0100

    correction

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,4 +1,3 @@
-export default function getStudentIdsSum(arr) {
-  return arr.reduce((acc, student) => acc + student.id, 0);
+export default function getStudentIdsSum(lst) {
+  return lst.reduce((counter, currentObj) => counter + currentObj.id, 0);
 }
-
